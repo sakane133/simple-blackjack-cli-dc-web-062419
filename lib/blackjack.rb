06c44077b7dab37633381cedbe8688a5474440a6
Samantha Kane <s.kane133@gmail.com>
2019-06-10 13:29:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,11 +33,11 @@ end
 def hit?(total)
   prompt_user
   user_input = get_user_input
- if user_input = "s"
-     total
-  elsif user_input = "h"
+  if user_input == "h"
    total+= deal_card
    total
+    elsif user_input == "s"
+     total
     else
     invalid_command
   end
