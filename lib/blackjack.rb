781fcff1796eb74574515aa7e26907e0e0601c43
Samantha Kane <s.kane133@gmail.com>
2019-06-10 12:58:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,11 +31,11 @@ def initial_round
   total
 end
 
-def hit?
+def hit?(total)
   prompt_user
   input = get_user_input
   if input = "h"
-    display_card_total
+    total+= deal_card
 end
 
 def invalid_command
